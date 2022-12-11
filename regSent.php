8207ea21fb651827adcff108f918d4ba8bcabda9
wokeupsnorlax <120267224+wokeupsnorlax@users.noreply.github.com>
2022-12-11 12:31:13 -0700

    PHP/HTML login and basic user profile for local host
    Skeleton framework to get started:
    home.php needs a "home feed"
    register.php needs to have the email sender changed
    register.html isn't needed/used but is framework for registration form -redundant
    profile.php needs basically everything

---
+++ regSent.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Thanks For Signing Up Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Thanks For Signing Up Page</h2>
+			<p></p>
+		</div>
+	</body>
+</html>
